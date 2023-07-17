d7480f524e7e24cb1857bcae36f67821fb4d9cbc
AndrzejZaba <andrzejzaba@onet.pl>
2023-07-17 14:57:12 +0200

    Add project files.

---
+++ PresentingPictures/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PresentingPictures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelectPicture = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnClearPicture = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectPicture
+            // 
+            this.btnSelectPicture.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSelectPicture.Location = new System.Drawing.Point(186, 12);
+            this.btnSelectPicture.Name = "btnSelectPicture";
+            this.btnSelectPicture.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPicture.TabIndex = 0;
+            this.btnSelectPicture.Text = "Select";
+            this.btnSelectPicture.UseVisualStyleBackColor = true;
+            this.btnSelectPicture.Click += new System.EventHandler(this.btnSelectPicture_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 348);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnClearPicture
+            // 
+            this.btnClearPicture.Location = new System.Drawing.Point(290, 12);
+            this.btnClearPicture.Name = "btnClearPicture";
+            this.btnClearPicture.Size = new System.Drawing.Size(75, 23);
+            this.btnClearPicture.TabIndex = 2;
+            this.btnClearPicture.Text = "Clear";
+            this.btnClearPicture.UseVisualStyleBackColor = true;
+            this.btnClearPicture.Click += new System.EventHandler(this.btnClearPicture_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.Filter = "\"Image Files(*.BMP;*.PNG;*.JPG;*.GIF)|*.BMP;*.JPG;*.PNG;*.GIF|All files (*.*)|*.*" +
+    ";\"";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.btnClearPicture);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSelectPicture);
+            this.MaximumSize = new System.Drawing.Size(600, 450);
+            this.MinimumSize = new System.Drawing.Size(600, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectPicture;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClearPicture;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ PresentingPictures/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PresentingPictures
+{
+    public partial class Form1 : Form
+    {
+        //private OpenFileDialog openFileDialog;
+        public string SelectedImagePath { get; set; }
+        public bool IsImageVisible { get; set; }
+        private readonly string textFilePath = "..\\..\\SavedImagePath.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            IsImageVisible = false;
+            try
+            {
+                SelectedImagePath = File.ReadAllText(textFilePath);
+            }
+            catch (Exception)
+            {
+                SelectedImagePath = "";
+            }
+
+            if (string.IsNullOrEmpty(SelectedImagePath))
+                IsImageVisible = false;
+            else
+                IsImageVisible = true;
+
+            RefreshWindow();
+        }
+
+        private void btnSelectPicture_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void RefreshWindow()
+        {
+            pictureBox1.ImageLocation = SelectedImagePath;
+            btnClearPicture.Enabled = IsImageVisible;
+        }
+
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(openFileDialog.FileName))
+            {
+                SelectedImagePath = openFileDialog.FileName;
+                File.WriteAllText(textFilePath, SelectedImagePath);
+                IsImageVisible = true;
+            }
+            RefreshWindow();
+        }
+
+        private void btnClearPicture_Click(object sender, EventArgs e)
+        {
+            SelectedImagePath = "";
+            File.WriteAllText(textFilePath, SelectedImagePath);
+            IsImageVisible = false;
+            RefreshWindow();
+        }
+    }
+}
